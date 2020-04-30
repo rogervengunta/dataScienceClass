2ba4573c94751620b17befee684b0f3ef2e3139f
Raj Vengunta <roger_vengunta@yahoo.com>
2020-04-30 18:22:22 -0500

    separate folders

--- ReverseEngineering.py
+++ ReverseEngineering.py
@@ -8,17 +8,18 @@ def funcRecursive(numArg): #FINDING THE FACTORIAL (BELOW fact() FUNC)
     Returns:
         [type] -- [description]
     """
-    print("1st line of fund def")
+    #print("1st line of fund def")
     if(numArg > 0): #base case
         result = numArg + funcRecursive(numArg-1) #recursive case
-        print("if block result:", result) #CALL STACK?? WHICH PART GOES INTO CALL STACK?
+        #print("if block result:", result) #CALL STACK?? WHICH PART GOES INTO CALL STACK?
     else:
         result = 0
-        print("else block result:", result)
+        #print("else block result:", result)
     return result
 
-print("\n \n Recursion example results:")
-funcRecursive(3) # why the heck is this printing the numbers it's printing?!!
+#print("\n \n Recursion example results:")
+x = funcRecursive(3) # why the heck is this printing the numbers it's printing?!!
+print(x)
 
 """
 def fact(n):
